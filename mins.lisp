051f904ab3b66bc71870a0168ead87df860fa0d4
prabhuvalluvan <prabhuvalluvan@gmail.com>
2020-04-15 12:09:52 -0400

    Update mins.lisp

--- mins.lisp
+++ mins.lisp
@@ -2,9 +2,9 @@
 	
 	(cond 
 	((or
-	   (not(and (listp lst1) (listp lst2) (listp lst3))) ;Check whether three args are list or not, return NIL if anyone args is not list
-	   (or (null lst1) (null lst2) (null lst3)) ;Check whether three lists are null, return NIL if anyone is null
-	   (not(= (length lst1) (length lst2) (length lst3)))) ;Check whether three list are of equal length, return NIL if its not
+	   (not(and (listp lst1) (listp lst2) (listp lst3)))   ;Check whether three args are list or not, return NIL if anyone args is not list
+	   (or (null lst1) (null lst2) (null lst3))   ;Check whether three lists are null, return NIL if anyone is null
+	   (not(= (length lst1) (length lst2) (length lst3))))   ;Check whether three list are of equal length, return NIL if its not
 	    nil)   
 	(t(let 
 		;;add element which is minimum of the cube of head of three lists to new list and call recursively on tail of three lists
