e1e3dbd322e318118b127c3bc52f5a91bdeecfc9
Ravi Kadiwala <rdkadiwala@users.noreply.github.com>
2020-04-16 17:21:22 -0400

    Added comment

--- diff.lisp
+++ diff.lisp
@@ -1,11 +1,10 @@
-
 (defun diff (lst1 lst2)
   (cond
-    ((or
-       (not (and (listp lst1) (listp lst2)))
-       (or (null lst1) (null lst2)) 
-       (not (= (length lst1) (length lst2)))) nil)
-    (t (let
-         ((d (- (car lst1) (car lst2))))
-         (cons (expt d 3) (diff (cdr lst1) (cdr lst2)))))))
+       ((not (and (listp lst1) (listp lst2))) nil) 		;To check whether list1 and list2 are list or not
+       ((or (null lst1) (null lst2)) nil) 			;To check whether list1 and list2 are null or not
+       ((not (= (length lst1) (length lst2))) nil) 		;To check whether lenght of list1 and list2 are equal or not
 
+       ;; Otherwise
+       (t (let
+         ((d (- (car lst1) (car lst2)))) 			;Take list1 and list2 head difference and store it in variable "d"
+         (cons (* d d d) (diff (cdr lst1) (cdr lst2))))))) 	;Cube variable d and add it in new list, call diff function on list1 and list2 tail recursivly
